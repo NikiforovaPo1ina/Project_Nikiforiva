7ca40c27e2a628cb38e5a2029facf6a52196d94e
Guchyara <mr.n.gushin@mail.ru>
2026-05-27 21:41:59 +0300

    Fix GitHub Actions workflow

--- backend/config.py
+++ backend/config.py
@@ -4,3 +4,5 @@ import os
 load_dotenv()
 
 HF_TOKEN = os.getenv("HF_TOKEN")
+API_URL = "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-schnell"
+HEADERS = {"Authorization": f"Bearer {HF_TOKEN}"}
