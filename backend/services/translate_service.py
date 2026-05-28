88f6973b71bce3df110fa8164c2887ffcc2f4414
Guchyara <mr.n.gushin@mail.ru>
2026-05-28 19:07:35 +0300

    ove translation and image generation to external s

--- backend/services/translate_service.py
+++ backend/services/translate_service.py
@@ -1,70 +1,42 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import requests
 from functools import lru_cache
 import logging
 import os
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-model = None
-tokenizer = None
-
-# Режим для прогона тестов
 TESTING = os.getenv("TESTING") == "1"
-
-def load_translator():
-
-    global model, tokenizer
-
-    # Если включен тестовый режим - выход
-    if TESTING:
-        return
-
-    if model is None or tokenizer is None:
-
-
-        logger.info("🌍 Загрузка переводчика...")
-
-        model_name = "Helsinki-NLP/opus-mt-ru-en"
-
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-        model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-
-        logger.info("✅ Переводчик загружен")
 
 
 @lru_cache(maxsize=100)
 def translate(text: str) -> str:
 
-    # Во время тестов просто возвращаем текст
     if TESTING:
         return text
+
     try:
 
-        load_translator()
+        logger.info(f"🌍 Перевод текста: {text[:50]}...")
 
-        logger.info(f"Перевод текста: {text[:50]}...")
-
-        inputs = tokenizer(
-            text,
-            return_tensors="pt",
-            truncation=True,
-            max_length=512
+        response = requests.post(
+            "https://libretranslate.de/translate",
+            json={
+                "q": text,
+                "source": "ru",
+                "target": "en",
+                "format": "text"
+            },
+            timeout=20
         )
 
-        outputs = model.generate(
-            **inputs,
-            max_length=512
-        )
+        result = response.json()
 
-        result = tokenizer.decode(
-            outputs[0],
-            skip_special_tokens=True
-        )
+        translated = result["translatedText"]
 
-        logger.info(f"Переведено как: {result[:50]}...")
+        logger.info(f"✅ Переведено: {translated[:50]}...")
 
-        return result
+        return translated
 
     except Exception as e:
 
